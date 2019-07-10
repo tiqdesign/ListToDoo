8cffe47a6025119a0e468c95d45ba124a03b867b
tiqdesign <tiqqq24@gmail.com>
2019-07-10 23:00:51 +0300

    updated

---
+++ ListToDoo/ListToDoo.Android/obj/Release/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -0,0 +1,14 @@
+package mono.android.app;
+
+public class XamarinAndroidEnvironmentVariables
+{
+	// Variables are specified the in "name", "value" pairs
+	public static final String[] Variables = new String[] {
+		"MONO_DEBUG", "gen-compact-seq-points",
+		"XAMARIN_BUILD_ID", "013e4601-c2e7-41fc-a2c1-beca48df4516",
+		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
+		"XA_TLS_PROVIDER", "btls",
+		"MONO_GC_PARAMS", "major=marksweep-conc",
+
+	};
+}
